80d3ad23b7470a5991d034e62763ce25d6009af0
Muhammad Imran <50030345+ilyasimran27@users.noreply.github.com>
2019-08-22 15:07:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var time = 0;
-var running = 0;
-
-function startPause(){
+let time = 0;
+let running = 0;
+let mins=0;
+let secs=0;
+let tenths=0;
+let fun=0;
+function start(){
 	if(running == 0){
 		running=1;
 		increment();
@@ -11,14 +14,18 @@ function startPause(){
 };
 
 function reset(){
+	clearInterval(fun);
 	running = 0;
 	time = 0;
-	document.getElementById("output").innerHTML = "00:00:00";
-	document.getElementById("start").innerHTML = "Start";
+	mins=0;
+	secs=0;
+	tenths=0;
+	document.getElementById("output").innerHTML ="00:00:00";
+	
 	document.getElementById("pause").innerHTML = "pause";
 };
 function pause(){
-	
+	clearInterval(fun);
 	if(running==1){
 		running=0;
 		document.getElementById("pause").innerHTML = "Resume";
@@ -33,22 +40,23 @@ function pause(){
 
 function increment(){
 	if(running == 1){
-		setTimeout(function(){
+fun=setInterval(function(){
 			time++;
-			var mins = Math.floor(time / 10 / 60);
+			 mins = Math.floor(time / 10 / 60);
 			if(mins <= 9){
 				mins = "0" + mins;
 			}
-			var secs = Math.floor(time / 10);
+			 secs = Math.floor(time / 10);
 			if(secs <= 9){
 				secs = "0" + secs;
 			}
-			var tenths = Math.floor(time % 10);
+			 tenths = Math.floor(time % 10);
 			if(tenths <= 9){
 				tenths = "0" + tenths;
 			}
 			document.getElementById("output").innerHTML = mins + ":" + secs + ":" + tenths;
-			increment();
+			
 		}, 100);
+		
 	}
 };
